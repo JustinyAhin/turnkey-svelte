d9ecc919a3f697a339d313b3bbc56b7a602e560c
Justin Ahinon <justiny.ahinon@gmail.com>
2025-05-30 07:06:29 +0300

    misc

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,2 @@
 <script>
-	import {
-		PUBLIC_GOOGLE_OAUTH_CLIENT_ID,
-		PUBLIC_TURNKEY_ORGANIZATION_ID
-	} from '$env/static/public';
-	import AuthDashboard from '$lib/components/dashboard/index.svelte';
-
-	const config = {
-		googleClientId: PUBLIC_GOOGLE_OAUTH_CLIENT_ID,
-		redirectURI: 'http://localhost:5173/',
-		turnkeyConfig: {
-			apiBaseUrl: 'https://api.turnkey.com',
-			defaultOrganizationId: PUBLIC_TURNKEY_ORGANIZATION_ID
-		}
-	};
 </script>
-
-<AuthDashboard
-	googleClientId={config.googleClientId}
-	redirectURI={config.redirectURI}
-	turnkeyConfig={config.turnkeyConfig}
-/>
