235f3efdb2a46a6ea979a36022ed602785517f5c
Justin Ahinon <justiny.ahinon@gmail.com>
2025-05-30 08:14:38 +0300

    Update +server.ts

--- src/routes/api/turnkey/google/+server.ts
+++ src/routes/api/turnkey/google/+server.ts
@@ -1,5 +1,5 @@
 import { TURNKEY_PRIVATE_KEY, TURNKEY_PUBLIC_KEY, TURNKEY_ORG_ID } from '$env/static/private';
-import { json } from '@sveltejs/kit';
+import { error, json } from '@sveltejs/kit';
 import { Turnkey } from '@turnkey/sdk-server';
 
 enum FilterType {
@@ -20,7 +20,10 @@ const client = turnkey.apiClient();
 
 export async function POST({ request }) {
 	try {
-		const { id_token: idToken, pubkey } = await request.json();
+		const { id_token: idToken, pubkey } = (await request.json()) as {
+			id_token: string | null;
+			pubkey: string | null;
+		};
 
 		if (!idToken || !pubkey) {
 			return json({ error: 'Missing parameters' }, { status: 400 });
@@ -31,10 +34,9 @@ export async function POST({ request }) {
 			filterValue: idToken
 		});
 
-		const suborgIds = subOrgsIds?.organizationIds;
-		if (!suborgIds?.length) {
-			console.error('suborg failure', suborgIds);
-			return json({ error: 'suborg failure' }, { status: 400 });
+		if (subOrgsIds?.organizationIds?.length > 0) {
+			console.error('Social login is already connected to another account', subOrgsIds);
+			throw error(400, 'Social login is already connected to another account');
 		}
 
 		// 2️⃣ Exchange the Google OIDC token for a Turnkey user session bound to the supplied pubkey
@@ -44,8 +46,8 @@ export async function POST({ request }) {
 		});
 
 		if (!loginResp?.session) {
-			console.error('oauthLogin failed', loginResp);
-			return json({ error: 'oauthLogin failed' }, { status: 400 });
+			console.error('Failed to login', loginResp);
+			throw error(400, 'Failed to login');
 		}
 
 		return json({ session: loginResp.session });
